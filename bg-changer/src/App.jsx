6aca025180f9cef6f71e815091f45f48ef526539
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-03-28 23:56:45 +0530

    .

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "red"}}
           >Red</button>
+          
           <button
           onClick={() => setColor("green")}
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
